7696c2108daecf20d5174f3d5b13d81003ea4056
Panagiotis <panververis@gmail.com>
2016-11-19 21:50:13 +0200

    Enabled Migrations

--- CarDealershipTest/Migrations/Configuration.cs
+++ CarDealershipTest/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace CarDealershipTest.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CarDealershipTest.Models.AreaContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<CarDealershipTest.Models.CarDealershipTestContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(CarDealershipTest.Models.AreaContext context)
+        protected override void Seed(CarDealershipTest.Models.CarDealershipTestContext context)
         {
             //  This method will be called after migrating to the latest version.
 
