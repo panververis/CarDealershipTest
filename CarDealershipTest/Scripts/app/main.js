e381d46c5f1cf1e82a0f882ad0b4c4ad365a95cf
Panagiotis <panververis@gmail.com>
2016-11-20 10:31:27 +0200

    first angular script commit

---
+++ CarDealershipTest/Scripts/app/main.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('main', main);
+
+    main.$inject = ['$scope']; 
+
+    function main($scope) {
+        $scope.food = 'pizza';
+
+        activate();
+
+        function activate() { }
+    }
+})();
