b4857bc8f94ca31befa7e7b2481143ca9a1bde98
Panagiotis <panververis@gmail.com>
2016-11-26 20:12:18 +0200

    -

--- CarDealershipTest/Scripts/app/main.js
+++ CarDealershipTest/Scripts/app/main.js
@@ -5,7 +5,12 @@
         .module('app', [])
         .service('SalesService', function($http) {
             this.getSales = function() {
-                return $http.get("/api/Sales");
+                //return $http.get("/api/Sales");
+                return $http({
+                    url: "/api/Sales",
+                    method: "GET"
+                    //,params: { param1: angular.toJson(myComplexObject, false) }
+                });
             }
         })
     .controller('main', ['SalesService', '$scope', function (salesService, $scope) {
