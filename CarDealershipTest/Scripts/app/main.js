22388ccfa7fe7beb442011e65079ae85780d9294
Panagiotis <panververis@gmail.com>
2016-12-03 15:59:07 +0200

    -

--- CarDealershipTest/Scripts/app/main.js
+++ CarDealershipTest/Scripts/app/main.js
@@ -8,35 +8,20 @@
                 var filterStr = angular.toJson(filterObject);
                 return $http.get("/api/Sales" + "/?filter=" + filterStr);
             }
-        }).directive('hcPieChart', function () {
-            return {
-                restrict: 'E',
-                template: '<div></div>',
-                scope: {
-                    title: '@',
-                    data: '='
-                },
+        })
+        .directive('hcPieChart', function () {
+            return { restrict: 'E', template: '<div></div>',
+                scope: { title: '@', data: '=' },
                 link: function (scope, element) {
                     Highcharts.chart(element[0], {
-                        chart: {
-                            type: 'pie'
-                        },
-                        title: {
-                            text: scope.title
-                        },
+                        chart: { type: 'pie'},
+                        title: {text: scope.title}, 
                         plotOptions: {
-                            pie: {
-                                allowPointSelect: true,
-                                cursor: 'pointer',
-                                dataLabels: {
-                                    enabled: true,
-                                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                                }
+                            pie: { allowPointSelect: true, cursor: 'pointer',
+                                dataLabels: { enabled: true, format: '<b>{point.name}</b>: {point.percentage:.1f} %' }
                             }
                         },
-                        series: [{
-                            data: scope.data
-                        }]
+                        series: [{ data: scope.data }]
                     });
                 }
             };
@@ -53,10 +38,10 @@
              { name: "Opera", y: 0.91 },
              { name: "Proprietary or Undetectable", y: 0.2 }]
 
+        //define the "Search" Function
         $scope.search = searchFunc;
 
         function searchFunc() {
-
             var filterObject = {
                 DateFrom: $scope.startDate,
                 DateTo: $scope.endDate,
@@ -66,12 +51,12 @@
             };
 
             var salesServCall = salesService.getSales(filterObject);
-                salesServCall.then(
-                    function (d) {
-                        var dData = d.data;
-                        $scope.sales = dData;
-                    }
-                );
+            salesServCall.then(
+                function (d) {
+                    var dData = d.data;
+                    $scope.sales = dData;
+                }
+            );
         }
 
     }]);
